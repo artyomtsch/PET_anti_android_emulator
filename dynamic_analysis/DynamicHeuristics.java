1db11be7e2303489286455670aef2050669c31df
artyomtsch <111195481+artyomtsch@users.noreply.github.com>
2024-04-25 20:15:31 +0300

    Update DynamicHeuristics.java

--- dynamic_analysis/DynamicHeuristics.java
+++ dynamic_analysis/DynamicHeuristics.java
@@ -1,1 +1,27 @@
+public class DynamicHeuristics {
+  // загрузка C-библиотеки  
+  static {
+       System.loadLibrary("bkpt");
+  }
 
+  // объявление native-функции из библиотеки
+  private native int qemuBkpt();
+
+  // проверка возможна только для архитектуры ARM
+  public boolean checkForBreakpointReaction() {
+    boolean isARM = false;
+    
+    for (String arch : android.os.Build.SUPPORTED_ABIS) {
+      if (arch.equalsIgnoreCase("armeabi-v7a")) {
+        isARM = true;
+        break;
+      }
+    }
+
+    if (isARM) {
+      return qemuBkpt() > 0;
+    }
+    
+    return false;
+  }
+}
